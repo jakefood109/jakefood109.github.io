7e59bfba11249127a285988c54aa047031c16cf6
jakefood109 <153372746+jakefood109@users.noreply.github.com>
2024-01-05 10:01:48 -0500

    Update weedcalculator.js
    Added expected profit input.

--- weedcalculator.js
+++ weedcalculator.js
@@ -2,8 +2,9 @@ function calculateWeed() {
     const dollars = parseFloat(document.getElementById('dollars').value);
     const stashprice = parseFloat(document.getElementById('stashprice').value);
     const stashamount = parseFloat(document.getElementById('stashamount').value);
+    const expectedProfit = parseFloat(document.getElementById('profit').value) || 0;
 
-    let weed = (stashamount / stashprice) * dollars;
+    let weed = (stashamount / stashprice) * dollars - (expectedProfit * (stashamount / stashprice));
     let resultElement = document.getElementById('result');
 
     if (weed > stashamount || isNaN(weed)) {
